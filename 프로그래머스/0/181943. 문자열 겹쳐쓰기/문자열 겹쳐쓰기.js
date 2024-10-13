83054b37586440cc6c81a820b2f86900ff550aaa
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-13 19:25:05 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -7,5 +7,7 @@ function solution(my_string, overwrite_string, s) {
             answer += my_string[i];
        }
     }
+    
+    // return my_string.slice(0,s) + overwrite_string + my_string.slice(s+overwrite_string.length);
     return answer;
 }
