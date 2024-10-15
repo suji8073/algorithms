3e8b8692de62887faf9535eff8551d9a2e0b55b0
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-15 15:50:15 +0900

    [level 1] Title: 기사단원의 무기, Time: 114.10 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,8 +1,9 @@
 function solution(number, limit, power) {
     let answer = 0;
-    for (var i=1; i<=number; i++) {
+    for (var i = 1; i <= number; i++) {
         let sum = 0;
-        for (var j=1; j<=Math.sqrt(i); j++){
+        
+        for (var j = 1; j <= Math.sqrt(i); j++){
             if (i % j === 0){
                 sum++;
                 if (j !== i/j){
