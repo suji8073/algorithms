2fbf765ad74eff79a6455398c382ab9d55150292
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-15 20:23:42 +0900

    [level 1] Title: 실패율, Time: 1781.80 ms, Memory: 46.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889#) 
 
 ### 성능 요약
 
-메모리: 51 MB, 시간: 1991.42 ms
+메모리: 46.2 MB, 시간: 1781.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 15일 17:52:36
+2024년 10월 15일 20:23:40
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -2,21 +2,18 @@ function solution(N, stages) {
     var array = [];
     let length = stages.length;
     
-    for (var i=1; i<=N; i++){
-        const count = stages.filter((stage)=> stage === i).length;
-        console.log(count, length)
-        array.push(length === 0 ? 0 : count/length);
+    for (var i = 0; i < N; i++){
+        const count = stages.filter((stage)=> stage === i + 1).length;
+        array.push(length === 0 ? 0 : count / length);
         length -= count;
     }
     
-    console.log(array)
     var answer = [];
-    for (var j = 0; j<array.length; j++){
+    for (var j = 0; j < array.length; j++){
         const maxIndex = array.findIndex((num) => num === Math.max(...array));
-        answer.push(maxIndex+1);
+        answer.push(maxIndex + 1);
         array[maxIndex] = -1;
     }
-
     
     return answer;
 }
