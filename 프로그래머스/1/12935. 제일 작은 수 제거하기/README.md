de09fd5bfd86faaff96e59248eab1d161dfd0992
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-15 21:45:22 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.43 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
 
 ### 성능 요약
 
-메모리: 43.6 MB, 시간: 2.58 ms
+메모리: 41.8 MB, 시간: 1.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 15일 21:29:20
+2024년 10월 15일 21:45:21
 
 ### 문제 설명
 
--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,5 +1,21 @@
 function solution(arr) {
-    const min = Math.min(...arr);
-    const array = arr.filter((num) => num !== min);
-    return array.length === 0 ? [-1] : array;
+    // const min = Math.min(...arr);
+    // const array = arr.filter((num) => num !== min);
+    // return array.length === 0 ? [-1] : array;
+    
+    if (arr.length === 1) return [-1];
+    
+    let min = Infinity;
+    let indexArr = 0;
+    
+    arr.forEach((value, index) => {
+        if (value < min) {
+            min = value;
+            indexArr = index;
+        } 
+    });
+    
+    arr.splice(indexArr, 1);
+    
+    return arr;
 }
