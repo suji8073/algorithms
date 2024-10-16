fde33ee15004f0757782d8d7eb0c1ed485154e60
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-16 09:56:51 +0900

    [level 1] Title: 내적, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,10 @@
 function solution(a, b) {
-    var answer = 0;
-    for (var i=0; i < a.length; i++){
-        answer += a[i] * b[i]
-    }
-    return answer;
+    // var answer = 0;
+    // for (var i=0; i < a.length; i++){
+    //     answer += a[i] * b[i]
+    // }
+    // return answer;
+    // 통과 (0.16ms, 33.5MB)
+    
+    return a.reduce((acc, cur, i) => acc + cur * b[i], 0)
 }
