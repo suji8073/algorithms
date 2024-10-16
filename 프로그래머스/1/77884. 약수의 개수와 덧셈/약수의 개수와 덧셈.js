e1f2aedf45edfe7c174afb544630723e063ecf45
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-16 10:21:43 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,14 +1,22 @@
 function solution(left, right) {
+//     var answer = 0;
+    
+//     for (var i = left; i <= right; i++){
+//         var count = 0;
+//         for (var j = 1; j <= Math.sqrt(i); j++){
+//             if (i % j === 0){
+//                 count += (i / j !== j ? 2 : 1)
+//             }
+//         }
+//         answer += (count % 2 === 0 ? i : -i);
+//     }
+    
+//     return answer;
+    
     var answer = 0;
     
-    for (var i = left; i <= right; i++){
-        var count = 0;
-        for (var j = 1; j <= Math.sqrt(i); j++){
-            if (i % j === 0){
-                count += (i / j !== j ? 2 : 1)
-            }
-        }
-        answer += (count % 2 === 0 ? i : -i);
+    for (var i = left; i <= right ; i++){
+        answer += (Number.isInteger(Math.sqrt(i)) ? -i : i)
     }
     
     return answer;
