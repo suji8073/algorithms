4d9b95feff79fc2cc814092c3959b70b6caec3a1
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-16 20:04:39 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.83 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,14 +2,10 @@ function solution(s) {
     var answer = [];
     
     [...s].forEach((char, i) => {
-        if (!s.slice(0, i).includes(char)){
-            answer.push(-1);
-        } else {
-            const index = [...s.slice(0,i)].reverse().findIndex((c) => c === char);
-            answer.push(index + 1);
-        }
-       
+        const count = s.slice(0,i).lastIndexOf(char);
+        answer.push(count < 0 ? count : i - count);
     })
 
     return answer;
 }
+
