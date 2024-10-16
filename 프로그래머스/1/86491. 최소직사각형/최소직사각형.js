88ff275724fdda92a1a2e5f9891082e5eb6af89f
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-16 19:18:36 +0900

    [level 1] Title: 최소직사각형, Time: 6.67 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,20 +1,36 @@
 function solution(sizes) {
-    const findMaxNum = Math.max(...sizes.flat());
-    const findMaxIndex = sizes.findIndex((size) => size[0] === findMaxNum || size[1] === findMaxNum);
+//     const findMaxNum = Math.max(...sizes.flat());
+//     const findMaxIndex = sizes.findIndex((size) => size[0] === findMaxNum || size[1] === findMaxNum);
     
-    let targetNum = Math.min(...sizes[findMaxIndex]);
+//     let targetNum = Math.min(...sizes[findMaxIndex]);
 
-    for (var i=0; i<sizes.length; i++){
-        if (i !== findMaxIndex){
-            const min = Math.min(...sizes[i]);
+//     for (var i=0; i<sizes.length; i++){
+//         if (i !== findMaxIndex){
+//             const min = Math.min(...sizes[i]);
             
-            if (targetNum < min){
-                targetNum = min;
-            }
+//             if (targetNum < min){
+//                 targetNum = min;
+//             }
+//         }
+//     }
+    
+//     return targetNum * findMaxNum;
+    
+    var array = sizes[0].sort((a,b) => b - a);
+    
+    for (var i = 1; i < sizes.length; i++) {
+        const size = sizes[i].sort((a, b) => b - a);
+        
+        if (array[0] < size[0]){
+            array[0]= size[0];
+        }
+        if (array[1] < size[1]){
+            array[1]= size[1];
         }
     }
     
-
-
-    return targetNum * findMaxNum;
+    return array[0] * array[1];
+    
+    
+   
 }
