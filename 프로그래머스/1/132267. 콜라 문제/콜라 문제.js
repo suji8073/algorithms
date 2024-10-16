f9dd817c49eb1c1f0f728a689120043bbe916166
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-16 21:32:50 +0900

    [level 1] Title: 콜라 문제, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -2,10 +2,8 @@ function solution(a, b, n) {
     var answer = 0;
     while (n  >= a && n >= 2) {
         const i = Math.floor(n / a);
-        const j = n - a * i;
-
         answer += i * b;
-        n = i * b + j;
+        n += (b - a) * i
     }
     return answer;
 }
