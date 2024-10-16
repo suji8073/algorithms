48b5d32f7b9efb6645f0c207663ca4e264349834
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-16 10:48:38 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    if (s.length !== 4 && s.length !== 6) return false;
+    for (var i = 0; i < s.length;  i++){
+        if (isNaN(s[i])){
+            return false;
+        }
+    }
+    
+    return true;
+}
