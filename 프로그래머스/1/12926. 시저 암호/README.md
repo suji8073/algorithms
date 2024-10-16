f8da62f29ff8ca456672cd867e86cbf44d2c0032
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-16 19:53:40 +0900

    [level 1] Title: 시저 암호, Time: 1.83 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 40.5 MB, 시간: 43.98 ms
+메모리: 37.5 MB, 시간: 1.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 16일 19:52:45
+2024년 10월 16일 19:53:38
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -5,14 +5,11 @@ function solution(s, n) {
         if (char === ' '){
             answer += ' ';
         } else if (char === char.toLowerCase()) {
-            // 소문자일 경우
-            console.log('a'.charCodeAt(), 'z'.charCodeAt())
             const num = char.charCodeAt() + n;
             answer += String.fromCharCode(num > 'z'.charCodeAt() ? num % 'z'.charCodeAt() + 'a'.charCodeAt() - 1 : num);
         } else {
-              console.log('A'.charCodeAt(), 'Z'.charCodeAt())
             const num = char.charCodeAt() + n;
-            answer += String.fromCharCode(num > 'Z'.charCodeAt() ? num % 'Z'.charCodeAt() + 'A'.charCodeAt() - 1: num);
+            answer += String.fromCharCode(num > 'Z'.charCodeAt() ? num % 'Z'.charCodeAt() + 'A'.charCodeAt() - 1 : num);
         }
     });
 
