c5aac6e3a7693d26d22e2435468f760a1fc079de
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-17 21:25:46 +0900

    [level 1] Title: 소수 만들기, Time: 6.12 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,30 +1,24 @@
+function checkNum(sum) {
+    for (var i = 2; i <= Math.sqrt(sum); i++) {
+        if (sum % i === 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function solution(nums) {
+    var answer = 0;
     const len = nums.length;
     
-    const result = [];
     for (var i = 0; i < len - 2; i++) {
         for (var j = i + 1; j < len - 1; j++){
             for (var k = j + 1; k < len; k++) {
                 const sum = nums[i] + nums[j] + nums[k];
-                result.push(sum);
+                if (!checkNum(sum)) answer ++;
             }
         }
     }
-    
-    var answer = 0;
-    result.forEach((num) => {
-        let check = false;
-        for (var i = 2; i <= Math.sqrt(num); i++){
-            if (num % i === 0) {
-                check = true;
-                break;
-            }
-        }
-        
-        if (!check) answer ++;
-    })
-    
-
     
     return answer;
 }
