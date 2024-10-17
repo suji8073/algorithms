39af564486b54ba9b8fc070a5195d4bf00253ac8
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-17 10:11:05 +0900

    [level 1] Title: 명예의 전당 (1), Time: 9.74 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,15 +1,18 @@
 function solution(k, score) {
     var answer = [];
     var stack = [];
-    for (var i=0; i<score.length; i++){
-        if (stack.length === k && Math.min(...stack) < score[i]){
-            stack[k-1] = score[i];
-        } else if (stack.length < k){
-            stack.push(score[i]); 
-        }
-        stack.sort((a, b) => b-a);
-        answer.push(Math.min(...stack));
-    }
     
+    for (var i = 0; i < score.length; i++) {
+        if (stack.length < k){
+            stack.push(score[i]);
+        } else {
+            if (stack[0] < score[i]){
+                stack.splice(0, 1);
+                stack.push(score[i]);
+            }
+        }
+        stack.sort((a,b) => a-b);
+        answer.push(stack[0]);
+    }
     return answer;
 }
