ac95aa0e371ee03e038c196d647d2073d00899bd
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-17 10:04:24 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,16 +1,12 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
+    var array = [];
     
     for (var i = 0; i < n; i++) {        
-        let num1 = arr1[i].toString(2).split('');
-        let num2 = arr2[i].toString(2).split('');
-        
-        num1 = Array(n-num1.length).fill('0').concat(num1);
-        num2 = Array(n-num2.length).fill('0').concat(num2);
-        
-        const array = num1.reduce((acc, cur, i) => acc + (cur === '0' && num2[i] === '0' ? 0 : 1), "")
-        answer.push([...array].map((num) => num === '1' ? '#': ' ').join(""));
+        const num1 = arr1[i].toString(2).padStart(n, '0');
+        const num2 = arr2[i].toString(2).padStart(n, '0');
+
+        array.push([...num1].reduce((acc, cur, i) => acc + (cur === '0' && num2[i] === '0' ? ' ' : '#'), ""));
     }
 
-    return answer;
+    return array;
 }
