8a5add583272df0134fb8e1abb01e67b5400e874
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-22 16:14:50 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,7 @@
+function solution(date1, date2) {
+    for (let i = 0; i < date1.length; i++) {
+        if (date1[i] < date2[i]) return 1;
+        else if (date1[i] > date2[i]) return 0;
+    }
+    return 0;
+}
