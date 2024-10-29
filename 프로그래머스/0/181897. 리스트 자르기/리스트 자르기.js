046e87090312ce3ed339074e396e88ed6a129aed
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-29 16:59:07 +0900

    [level 0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -2,9 +2,5 @@ function solution(n, slicer, num_list) {
     if (n === 1) return num_list.slice(0, slicer[1]+1);
     if (n === 2) return num_list.slice(slicer[0]);
     if (n === 3) return num_list.slice(slicer[0], slicer[1]+1);
-    if (n === 4) return num_list.filter((num, i) => {
-        if (i >= slicer[0] && i<=slicer[1] && (i + slicer[0]) % slicer[2] === 0) return true;
-        return false;
-    })
-
+    if (n === 4) return num_list.slice(slicer[0], slicer[1]+1).filter((_, i) => i % slicer[2] === 0);
 }
