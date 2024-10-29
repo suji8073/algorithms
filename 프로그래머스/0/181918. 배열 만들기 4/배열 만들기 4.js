9827ea2ad3215a1b6f2bbc05b8bc2700d08676a6
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-29 19:36:12 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.94 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+    var stk = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        const len = stk.length;
+        
+        if (len === 0) {
+            stk.push(arr[i]);
+            continue;
+        }
+        
+        if (stk[len-1] < arr[i]) stk.push(arr[i]);
+        else {
+            stk.pop();
+            i--;
+        }
+    }
+    
+    return stk;
+}
