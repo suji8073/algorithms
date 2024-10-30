a3abc9b25def965272a83407fb76e7cb16da3a5d
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-10-31 00:58:35 +0900

    [level 2] Title: 귤 고르기, Time: 82.78 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,29 +1,43 @@
-function solution(k, tangerine) {
-    var hashMap = new Map();
+// function solution(k, tangerine) {
+//     var hashMap = new Map();
     
-    tangerine.map(k => {
-        if (!hashMap.has(k)) {
-            hashMap.set(k, 1)
-        } else {
-            hashMap.set(k, hashMap.get(k) + 1);
-        }
-    })
+//     tangerine.map(k => {
+//         if (!hashMap.has(k)) {
+//             hashMap.set(k, 1)
+//         } else {
+//             hashMap.set(k, hashMap.get(k) + 1);
+//         }
+//     })
     
-    var array = [];
-    for (const [key, value] of hashMap) {
-        array.push([key, value])
-    }
+//     var array = [];
+//     for (const [key, value] of hashMap) {
+//         array.push([key, value])
+//     }
     
-    array.sort((a, b) => b[1] - a[1]);
+//     array.sort((a, b) => b[1] - a[1]);
     
-    for (let i = 0; i < array.length; i++) {
-        const [num, count] = array[i];
-        if (count >= k) {
-            return i + 1;
-        } else {
-            k -= count;
-        }
-    }
+//     for (let i = 0; i < array.length; i++) {
+//         const [num, count] = array[i];
+//         if (count >= k) {
+//             return i + 1;
+//         } else {
+//             k -= count;
+//         }
+//     }
    
-    return tangerine.length;
+//     return tangerine.length;
+// }
+
+function solution(k, tangerine) {
+    var result = {};
+    tangerine.map(k => result[k] ? result[k]++ : result[k] = 1);
+    
+    const count = Object.values(result).sort((a, b) => b-a);
+    
+    for (let i = 0; i < count.length; i++) {
+        if (count[i] >= k) {
+            return i + 1;
+        } 
+        k -= count[i];
+    }
 }
