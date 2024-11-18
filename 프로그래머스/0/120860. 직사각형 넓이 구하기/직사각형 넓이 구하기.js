c49ca30b511b2e63aeeb41cba6d3a3e1bde28201
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-11-18 15:49:08 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,6 +1,6 @@
 function solution(dots) {
-    const widthArray = [...new Set([dots[0][0], dots[1][0], dots[2][0], dots[3][0]])];
-    const heightArray = [...new Set([dots[0][1], dots[1][1], dots[2][1], dots[3][1]])];
+    const widthArr = [...new Set(dots.map(d => d[0]))];
+    const heightArr = [...new Set(dots.map(d => d[1]))];
 
-    return Math.abs(widthArray[0] - widthArray[1]) * Math.abs(heightArray[0] - heightArray[1]);
+    return Math.abs(widthArr[0] - widthArr[1]) * Math.abs(heightArr[0] - heightArr[1]);
 }
