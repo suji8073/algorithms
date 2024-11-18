b1d51bd68b46a22c0b0bdf309afa54d1542a6b45
Suji Chae <63655826+suji8073@users.noreply.github.com>
2024-11-18 15:09:23 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,19 +1,20 @@
 function solution(keyinput, board) {
-    board = [(board[0] - 1) / 2, (board[1] - 1) / 2];
+    const width = (board[0] - 1) / 2;
+    const height = (board[1] - 1) / 2;
     
     var answer = [0,0];
-    for (key of keyinput){
+    for (key of keyinput) {
         if (key === 'left') answer[0]--;
         else if (key === 'right') answer[0]++;
         else if (key === 'up') answer[1]++;
         else if (key === 'down') answer[1]--;
         
-        if (-board[0] > answer[0] || answer[0] > board[0]){
-            answer[0] =  answer[0] < 0 ? -board[0] : board[0];
+        if (-width > answer[0] || answer[0] > width){
+            answer[0] = answer[0] < 0 ? -width : width;
         }
         
-         if (-board[1] > answer[1] || answer[1] > board[1]){
-            answer[1] = answer[1] < 0 ? -board[1] : board[1];
+         if (-height > answer[1] || answer[1] > height){
+            answer[1] = answer[1] < 0 ? -height : height;
         }
     }
 
