207e715979be329ed5519896f5aa3529a9e49383
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-08 15:58:52 +0900

    [level 1] Title: 체육복, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,6 +1,7 @@
 function solution(n, lost, reserve) {
     const arr = Array(n).fill(1);
-
+    lost.sort();
+    reserve.sort();
     lost.map(l => arr[l-1]--);
     reserve.map(r => arr[r-1]++);
     
@@ -20,4 +21,30 @@ function solution(n, lost, reserve) {
     }
     
     return arr.filter(a => a!== 0).length;
+}
+
+
+function solution(n, lost, reserve) {
+    const realLost = lost.filter(l => !reserve.includes(l)).sort();
+    const realReserve = reserve.filter(r => !lost.includes(r)).sort();
+    
+    let lostNum = realLost.length;
+
+    for (const num of realLost) {
+        let index = realReserve.findIndex(r => r === num - 1);
+        if (index !== -1) {
+            realReserve.splice(index, 1);
+            lostNum--;
+            continue;
+        }
+        
+        index = realReserve.findIndex(r => r === num + 1);
+        if (index !== -1) {
+            realReserve.splice(index, 1);
+            lostNum--;
+            continue;
+        }
+    }
+    
+    return n - lostNum;
 }
