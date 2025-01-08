f73cb150c1d3a5754e3a60cc3a068116303e7e86
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-09 00:55:37 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,33 @@
+function solution(board, moves) {
+    var answer = 0;
+    var stack = [];
+    var numArr = [];
+    
+    const len = board.length;
+    for (let i = 0; i < len; i++) {
+        const temp = [];
+        for (let j = 0; j < len; j++) {
+            if (board[j][i] !== 0){
+                temp.push(board[j][i])
+            }
+        }
+        numArr.push(temp)
+    }
+
+   
+    for (const move of moves) {
+        
+        let target = numArr[move-1].shift();
+        if (target === undefined) continue;
+        if (stack.at(-1) === target){
+            answer += 2;
+            stack.pop();
+        } else {
+            stack.push(target);
+        }
+       
+    }
+    
+
+    return answer;
+}
