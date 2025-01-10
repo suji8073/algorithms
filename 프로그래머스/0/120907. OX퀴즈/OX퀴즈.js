97539ef20c08efd4d17e5d4d27deda1a0cf20704
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-10 11:02:54 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,12 +1,8 @@
 function solution(quiz) {
     return quiz.map((str) => {
         const [num1, calc, num2, _, num3] = str.split(' ');
-        
-        if (calc === '+' && +num1 + +num2 === +num3){
+        if (+num1 + +num2 * (calc === '+' ? 1 : -1) === +num3){
             return "O"
-        }
-        if (calc === '-' && +num1 - +num2 === +num3) {
-             return "O"
         }
         return "X"
     })
