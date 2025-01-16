a5adf4b5cb4ed3cf86dcd3982de7b04d4d8560c2
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-17 02:53:22 +0900

    [level 0] Title: 특이한 정렬, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,17 +1,19 @@
 function solution(numlist, n) {
-    const array = numlist.map((num) => ({dist : Math.abs(num - n), num}));
-    array.sort((a, b) => a.dist - b.dist);
+    return numlist.sort((a, b) => Math.abs(a-n) - Math.abs(b-n) || b - a);
     
-    const result = Array(array.length).fill(0);
-    for (let i = 0; i < array.length; i++){
-        if (i < array.length -1 && array[i].dist === array[i+1].dist){
-            result[i] = Math.max(array[i].num, array[i+1].num);
-            result[i+1] = Math.min(array[i].num, array[i+1].num);
-            i++;
-        } else {
-            result[i] = array[i].num;
-        }
-    }
+//     const array = numlist.map((num) => ({dist : Math.abs(num - n), num}));
+//     array.sort((a, b) => a.dist - b.dist);
+    
+//     const result = Array(array.length).fill(0);
+//     for (let i = 0; i < array.length; i++){
+//         if (i < array.length -1 && array[i].dist === array[i+1].dist){
+//             result[i] = Math.max(array[i].num, array[i+1].num);
+//             result[i+1] = Math.min(array[i].num, array[i+1].num);
+//             i++;
+//         } else {
+//             result[i] = array[i].num;
+//         }
+//     }
 
-    return result;
+//     return result;
 }
