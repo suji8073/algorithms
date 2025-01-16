349cbcb6660309b308385bcf3f045fe2506cd671
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-17 02:37:07 +0900

    [level 0] Title: 문자열 밀기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,13 @@
+function solution(A, B) {
+    
+    const len = A.length;
+    for (let i = 0; i < len; i++){
+        if (A === B) {
+            return i;
+        }
+        A = A[len-1] + A.slice(0, len-1);
+    }
+    
+    return -1;
+  
+}
