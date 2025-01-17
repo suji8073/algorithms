48684840ece538c0dbd862cfa40c8a6fe6357b94
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-17 16:20:57 +0900

    [level 0] Title: 다항식 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -2,16 +2,14 @@ function solution(polynomial) {
     var answer = [0, 0];
     
     polynomial.split(' + ').map((char) => {
-        if (!isNaN(char)){
-            answer[1] += +char;
+        if (char.includes('x')){
+            answer[0] += char === 'x' ? 1 : parseInt(char.replace('x', ''));
         } else {
-            const num = +char.slice(0, char.length-1);
-            answer[0] += num === 0 ? 1 : num;
+            answer[1] += parseInt(char)
         }
     });
     
     if (answer[0] === 0) return String(answer[1]);
     if (answer[1] === 0) return `${answer[0] === 1 ? '' : answer[0]}x`
-    
     return `${answer[0] === 1 ? '' : answer[0]}x + ${answer[1]}`
 }
