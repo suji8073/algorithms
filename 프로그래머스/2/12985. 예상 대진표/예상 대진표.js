67ac75a401467127bf65bedc2db7aa07d67d772a
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-20 00:31:22 +0900

    [level 2] Title: 예상 대진표, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,27 +1,10 @@
 function solution(n,a,b) {
-    var array = Array.from({length: n}, (_, i) => 1 + i);
-    
-    const ab = [a, b].sort((a, b) => a - b);
-    let count = 1;
-    
-    while(true) {
-        var oldArr = [];
-        for (let i = 0; i < array.length; i+= 2) {
-            if (array[i] === ab[0] && array[i+1] === ab[1]){
-                return count;
-            }
-            
-            if (!ab.includes(array[i]) && !ab.includes(array[i+1])){
-                oldArr.push(array[i]);
-                continue;
-            }
-            
-            oldArr.push(ab.includes(array[i]) ? array[i] : array[i+1])
-        }
-        
+    var count = 0;
+    while (a != b){
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
         count++;
-        array = oldArr;
     }
-
+    
     return count;
 }
