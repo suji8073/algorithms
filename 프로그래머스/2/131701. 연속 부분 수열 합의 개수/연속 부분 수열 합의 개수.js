ba6a6ea03ecc1b333ff8818c37b0c178ee093950
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-20 00:08:34 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1150.35 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,16 +1,13 @@
 function solution(elements) {
-    var answer = [];
     const len = elements.length;
+    elements = elements.concat(elements);
     
-    elements = [...elements, ...elements]
-
-
+    var answer = [];
     for (let i = 0; i <= len - 1; i++) {
         for (let j = 0; j <= len; j++){
             answer.push(elements.slice(i, i + j).reduce((acc, cur) => acc + cur, 0));
         }
     }
-    
     
     return [...new Set(answer)].length - 1;
 }
