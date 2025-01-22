d64246f2b1e9eb24245e92d35297d5ba29de31a5
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-01-22 14:39:03 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 18.16 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,33 +1,41 @@
 function solution(friends, gifts) {
-    var answer = 0;
-    const map = new Map(friends.map(friend => [friend, {...Object.fromEntries(friends.map(name => [name, 0])), count: 0}]));
+    const map = new Map();
+    const checkMap = new Map();
+    
+    const initialObject = Object.fromEntries(friends.map(name => [name, 0]));
+    
+    friends.forEach(friend => {
+        map.set(friend, {...initialObject, count: 0});
+        checkMap.set(friend, 0)
+    });
     
     for (const gift of gifts){
         const [people1, people2] = gift.split(' ');
         const target = map.get(people1);
-        map.get(people1)[people2]++;
-        map.get(people1)['count']++;
+        
+        target[people2]++;
+        target['count']++;
         map.get(people2)['count']--;
     }
-    const checkMap = new Map(friends.map(friend => [friend, 0]));
     
     for (let i = 0; i < friends.length - 1; i++) {
         const target = map.get(friends[i]);
         
         for (let j = i + 1; j < friends.length; j++) {
             const other = map.get(friends[j]);
-            
             const num1 = target[friends[j]];
             const num2 = other[friends[i]];
             
-            if (num1 === num2) {
-                if (target['count'] > other['count']) checkMap.set(friends[i], checkMap.get(friends[i]) + 1)
-                else if (target['count'] < other['count']) checkMap.set(friends[j], checkMap.get(friends[j]) + 1)
+            if (num1 !== num2) {
+                const winner = num1 > num2 ? friends[i] : friends[j];
+                checkMap.set(winner, checkMap.get(winner) + 1);
                 continue;
             }
             
-            if (num1 > num2) checkMap.set(friends[i], checkMap.get(friends[i]) + 1)
-            else checkMap.set(friends[j], checkMap.get(friends[j]) + 1)
+            if (target['count'] === other['count']) continue;
+            
+            const winner = target['count'] > other['count'] ? friends[i] : friends[j];
+            checkMap.set(winner, checkMap.get(winner) + 1);
         }
     }
     
