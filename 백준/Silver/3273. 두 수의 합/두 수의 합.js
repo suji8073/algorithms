0693a5c3ca346b81945858bf59da0dfb7c56cb61
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-02-07 22:32:23 +0900

    [Silver III] Title: 두 수의 합, Time: 192 ms, Memory: 18348 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.js
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -1,14 +1,32 @@
 const fs = require('fs');
-let inputFile = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
+let inputFile = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 
 const n = Number(inputFile[0]);
-const arr = inputFile[1].split(' ').map(Number);
-const target = Number(inputFile[2]);
+const arr = inputFile[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const k = Number(inputFile[2]);
 
-let result = 0;
-for (let i = 0; i < n - 1; i++) {
-  for (let j = i + 1; j < n; j++) {
-    if (arr[i] + arr[j] === target) result++;
+function getCount() {
+  let count = 0;
+  let left = 0;
+  let right = n - 1;
+
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+
+    if (sum === k) {
+      count++;
+      left++;
+      right--;
+    } else if (sum > k) {
+      right--;
+    } else {
+        left++;
+    }
   }
+  return count;
 }
-console.log(result);
+
+console.log(getCount());
