dc14b2ff69ad4e066e13f3a7ecd714cd34b337fb
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-02-20 01:50:03 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9580 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 const [n, input] = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
 
+const MOD = 1234567891;
 const arr = input.split('').map((char) => char.charCodeAt() - 97 + 1);
 
 let sum = 0;
-for (let i = 0; i < n; i++) {
-  const num = 31 ** i * arr[i];
-  sum += num;
+let power = 1; 
+
+for (let i = 0; i < Number(n); i++) {
+  sum = (sum + (arr[i] * power) % MOD) % MOD;
+  power = (power * 31) % MOD;
 }
+
 console.log(sum);
