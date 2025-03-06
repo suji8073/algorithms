a7d0c32b38c23714b299638bf7b1e7322ae416e8
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-03-06 19:55:16 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 436 ms, Memory: 59400 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const [_, ...input] = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
+const arr = input.map((row) => row.split(' ').map(Number));
+
+arr.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+console.log(arr.map((row) => row.join(' ')).join('\n'));
