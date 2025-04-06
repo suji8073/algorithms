42de3b0784373436257633ccc9b953261014b925
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-06 16:23:19 +0900

    [level 1] Title: 모의고사, Time: 5.22 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 25.09 ms
+메모리: 36.9 MB, 시간: 5.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 17일 20:53:15
+2025년 04월 06일 16:23:16
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,24 +1,28 @@
 function solution(answers) {
-    const person1 = [1,2,3,4,5];
-    const person2 = [2,1,2,3,2,4,2,5];
-    const person3 = [3,3,1,1,2,2,4,4,5,5];
+    const array = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ];
     
-    
-    const result = [0,0,0];
-    for (var i = 0; i < answers.length; i++) {
-        if (person1[i % person1.length] === answers[i]) result[0]++;
-        if (person2[i % person2.length] === answers[i]) result[1]++;
-        if (person3[i % person3.length] === answers[i]) result[2]++;
-    }
-
-    const max = Math.max(...result);
-    let answer = [];
-    
-    result.forEach((r, i) => {
-        if (r === max){
-            answer.push(i+1);
+    const num = Math.floor(answers.length / 5) + 1;
+    const resultArr = [];
+    array.forEach((arr, index) => {
+        const newAnswers = [...answers];
+        
+        let count = 0;
+        while (newAnswers.length > 0){
+            const target = newAnswers.splice(0, arr.length);
+            const len = target.filter((t, i) => t === arr[i]).length;
+            count+= len;
         }
+        resultArr.push(count);
     });
     
-    return answer;
+    const max = Math.max(...resultArr);
+    const result = [];
+    resultArr.map((r, i) => {
+        if (r === max) result.push(i+1);
+    })
+    return result;
 }
