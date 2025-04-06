2a245632f95524883a86063c578fc4a4175b5435
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-06 15:52:19 +0900

    [level 1] Title: 최소직사각형, Time: 24.22 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,36 +1,12 @@
 function solution(sizes) {
-//     const findMaxNum = Math.max(...sizes.flat());
-//     const findMaxIndex = sizes.findIndex((size) => size[0] === findMaxNum || size[1] === findMaxNum);
+    let x = 0;
+    let h = 0;
     
-//     let targetNum = Math.min(...sizes[findMaxIndex]);
-
-//     for (var i=0; i<sizes.length; i++){
-//         if (i !== findMaxIndex){
-//             const min = Math.min(...sizes[i]);
-            
-//             if (targetNum < min){
-//                 targetNum = min;
-//             }
-//         }
-//     }
+    sizes.forEach((row) => {
+        const size = row.sort((a, b) => a - b);
+        x = Math.max(x, size[0]);
+        h = Math.max(h, size[1]);
+    });
     
-//     return targetNum * findMaxNum;
-    
-    var array = sizes[0].sort((a,b) => b - a);
-    
-    for (var i = 1; i < sizes.length; i++) {
-        const size = sizes[i].sort((a, b) => b - a);
-        
-        if (array[0] < size[0]){
-            array[0]= size[0];
-        }
-        if (array[1] < size[1]){
-            array[1]= size[1];
-        }
-    }
-    
-    return array[0] * array[1];
-    
-    
-   
+    return x * h;
 }
