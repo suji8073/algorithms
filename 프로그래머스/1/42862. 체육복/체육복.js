87de9e743c5cdbf5e13498945c877666cdc1ee8e
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-08 14:50:57 +0900

    [level 1] Title: 체육복, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,49 +1,20 @@
 function solution(n, lost, reserve) {
     const arr = Array(n).fill(1);
-
+    
     lost.map(l => arr[l-1]--);
     reserve.map(r => arr[r-1]++);
-    
-    for (let i = 0; i < arr.length; i++) {
+
+    for (let i = 0; i < n; i++) {
         if (arr[i] !== 0) continue;
         
-        if (i !== 0 && arr[i-1] === 2){
+        if (i !== 0 && arr[i-1] > 1) {
             arr[i-1]--;
             arr[i]++;
-            continue;
-        }
-        
-        if (i !== arr.length - 1 && arr[i+1] === 2){
+        } else if (i + 1 < n && arr[i+1] > 1){
             arr[i+1]--;
             arr[i]++;
         }
     }
     
-    return arr.filter(a => a!== 0).length;
-}
-
-
-function solution(n, lost, reserve) {
-    const realLost = lost.filter(l => !reserve.includes(l)).sort();
-    const realReserve = reserve.filter(r => !lost.includes(r)).sort();
-    
-    let lostNum = realLost.length;
-
-    for (const num of realLost) {
-        let index = realReserve.findIndex(r => r === num - 1);
-        if (index !== -1) {
-            realReserve.splice(index, 1);
-            lostNum--;
-            continue;
-        }
-        
-        index = realReserve.findIndex(r => r === num + 1);
-        if (index !== -1) {
-            realReserve.splice(index, 1);
-            lostNum--;
-            continue;
-        }
-    }
-    
-    return n - lostNum;
+    return arr.filter(n => n >= 1).length;
 }
