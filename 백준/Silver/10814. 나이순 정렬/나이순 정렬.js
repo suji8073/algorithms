cf5a628cbaf202c46672a021594eb553d4fc7344
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-10 02:04:14 +0900

    [Silver V] Title: 나이순 정렬, Time: 348 ms, Memory: 44120 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-const inputFile = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
+const input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
 
-const arr = inputFile.slice(1).map((row) => row.split(' '));
+const arr = input.slice(1).map((row) => row.split(' '));
 
-arr.forEach((row, i) => row.push(i));
-arr.sort((a, b) => Number(a[0]) - Number(b[0]) || a[2] - b[2]);
-
-arr.forEach((row) => console.log(row[0] + ' ' + row[1]));
+arr.sort((a, b) => +a[0] - +b[0]);
+console.log(arr.map((row) => row.join(' ')).join('\n'));
