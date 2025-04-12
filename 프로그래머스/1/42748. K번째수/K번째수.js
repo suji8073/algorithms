27fab31e4ea15be11d90d5edacd6e9d5ddcd909d
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-12 14:44:45 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,6 @@
 function solution(array, commands) {
-    const result = [];
-    for (const [start, end, index] of commands){
-        const newArr = array.slice(start - 1, end).sort((a, b) => a - b)
-        result.push(newArr[index-1]);
-    }
-    return result;
+    return commands.map(([i, j, k]) => {
+        const arr = [...array.slice(i - 1, j)].sort((a, b) => a - b);
+        return arr[k-1];
+    });
 }
