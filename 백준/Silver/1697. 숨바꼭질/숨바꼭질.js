f3e15c6d08568b4164f1631185f3f35ef1d9d436
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-12 15:18:10 +0900

    [Silver I] Title: 숨바꼭질, Time: 3624 ms, Memory: 22404 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,31 +1,31 @@
 const fs = require('fs');
+const [N, K] = fs.readFileSync('/dev/stdin', 'utf-8').trim().split(' ').map(Number);
 
-const [start, end] = fs
-  .readFileSync('/dev/stdin', 'utf8')
-  .trim()
-  .split(' ')
-  .map(Number);
-
+const MIN = 0;
 const MAX = 100000;
 
-console.log(bfs(start, end));
+const visited = Array(MAX).fill(false);
 
-function bfs(start, end) {
-  const queue = [[start, 0]];
-  const visited = Array(MAX + 1).fill(false);
+bfs();
 
-  visited[start] = true;
+function bfs() {
+  const queue = [[N, 0]];
+  visited[N] = true;
 
-  while (queue.length > 0) {
-    const [current, count] = queue.shift();
-    if (current === end) return count;
+  while (queue.length >= 0) {
+    const [location, count] = queue.shift();
 
-    const nextNumArr = [current + 1, current - 1, current * 2];
-    for (const next of nextNumArr) {
-      if (next >= 0 && next <= MAX && !visited[next]) {
-        visited[next] = true;
-        queue.push([next, count + 1]);
-      }
+    if (location === K) {
+      console.log(count);
+      return;
     }
+
+    const numbers = [location + 1, location - 1, location * 2];
+    numbers.forEach((num) => {
+      if (num >= MIN && num <= MAX && !visited[num]) {
+        visited[num] = true;
+        queue.push([num, count + 1]);
+      }
+    });
   }
 }
