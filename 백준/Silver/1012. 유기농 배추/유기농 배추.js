c353bda5780c660fa002944313bf59e4c34a313c
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-12 18:00:48 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 12448 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,50 +1,37 @@
 const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
+const [T, ...input] = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
 
-const vectors = [
-  [-1, 0],
-  [1, 0],
-  [0, -1],
-  [0, 1],
-];
+let index = 0;
+for (let i = 0; i < Number(T); i++) {
+  const [M, N, K] = input[index].split(' ').map(Number);
+  const arr = input
+    .slice(index + 1, index + 1 + K)
+    .map((row) => row.split(' ').map(Number));
+  index += K + 1;
 
-input = input.slice(1);
-while (input.length > 0) {
-  const [m, n, k] = input[0].split(' ').map(Number);
-  const array = input.slice(1, k + 1).map((row) => row.split(' ').map(Number));
-
-  const graph = Array.from({ length: n }, () => Array(m).fill(0));
-  for (const [x, y] of array) {
-    graph[y][x] = 1;
-  }
+  const visited = Array.from({ length: M }, () => Array(N).fill(false));
+  arr.forEach(([x, y]) => (visited[x][y] = true));
 
   let count = 0;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (graph[i][j] === 1) {
-        dfs(i, j);
+  for (let i = 0; i < M; i++) {
+    for (let j = 0; j < N; j++) {
+      if (visited[i][j] === true) {
         count++;
-      }
-    }
-  }
-
-  function dfs(x, y) {
-    if (x < 0 || x >= n || y < 0 || y >= m) return;
-    if (graph[x][y] === 0) return;
-
-    graph[x][y] = 0;
-
-    for (const vector of vectors) {
-      const nx = x + vector[0];
-      const ny = y + vector[1];
-
-      if (nx >= 0 && nx < n && ny >= 0 && ny < m && graph[nx][ny] === 1) {
-        dfs(nx, ny);
+        dfs(i, j);
       }
     }
   }
 
   console.log(count);
 
-  input = input.slice(k + 1);
+  function dfs(start, end) {
+    if (start < 0 || start >= M || end < 0 || end >= N || !visited[start][end])
+      return;
+
+    visited[start][end] = false;
+    dfs(start - 1, end);
+    dfs(start + 1, end);
+    dfs(start, end - 1);
+    dfs(start, end + 1);
+  }
 }
