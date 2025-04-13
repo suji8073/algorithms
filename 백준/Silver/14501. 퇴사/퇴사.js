86fefc54e4d076b14ffea0a72bc1f31c5a83abb2
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-13 22:27:07 +0900

    [Silver III] Title: 퇴사, Time: 156 ms, Memory: 10808 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -5,26 +5,43 @@ const N = Number(n);
 const arr = input.map((row) => row.split(' ').map(Number));
 
 let max = 0;
-const paths = [];
-backtrack(0, []);
-
+backtrack(0, 0);
 console.log(max);
 
-function backtrack(start) {
+function backtrack(start, sum) {
   if (start >= N) {
-    const sum = paths.reduce((acc, cur) => acc + cur, 0);
     max = Math.max(max, sum);
     return;
   }
 
-  for (let i = start; i < N; i++) {
-    if (arr[i][0] + i <= N) {
-      paths.push(arr[i][1]);
-      backtrack(arr[i][0] + i);
-      paths.pop(); // 백트래킹 핵심!
-    } else {
-      const sum = paths.reduce((acc, cur) => acc + cur, 0);
-      max = Math.max(max, sum);
-    }
+  const [t, p] = arr[start];
+
+  if (start + t <= N) {
+    backtrack(start + t, sum + p);
   }
+  backtrack(start + 1, sum);
 }
+
+// let max = 0;
+// const paths = [];
+// backtrack(0, []);
+
+// console.log(max);
+// function backtrack(start) {
+//   if (start >= N) {
+//     const sum = paths.reduce((acc, cur) => acc + cur, 0);
+//     max = Math.max(max, sum);
+//     return;
+//   }
+
+//   for (let i = start; i < N; i++) {
+//     if (arr[i][0] + i <= N) {
+//       paths.push(arr[i][1]);
+//       backtrack(arr[i][0] + i);
+//       paths.pop(); // 백트래킹 핵심!
+//     } else {
+//       const sum = paths.reduce((acc, cur) => acc + cur, 0);
+//       max = Math.max(max, sum);
+//     }
+//   }
+// }
