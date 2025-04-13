9b94d4a957e24eb94e7f4a113e4977ff3ad66a8d
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-13 17:22:17 +0900

    [level 3] Title: 단어 변환, Time: 2.80 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -4,46 +4,38 @@ function solution(begin, target, words) {
     
     let min = null;
     dfs(begin, 0, wordKey);
+    
     return min ?? 0;
     
     function dfs(start, count, key) {
-        console.log(start, count, key)
         if (start === target) {
-            if (!min) min = count;
-            else min = Math.min(min, count);
+            min = !min ? count : Math.min(min, count);
             return;
         }
         
         for (const word of words) {
             if (!key.has(word)) continue;
-            const wordStr = [...word];
-            // const length = [...start].filter(s => !(wordStr.includes(s))).length;
+            
             const length = checkStr(start, word);
-            console.log("@@", word, length, start)
-            if (length === 1) {
-                console.log("@@@@word", word)
-                const newKey = new Set(key);
-                newKey.delete(word);
-                dfs(word, count + 1, newKey)
-            }
+            if (length !== 1) continue;
+            
+            const newKey = new Set(key);
+            newKey.delete(word);
+            dfs(word, count + 1, newKey)
         }
-        
     }
-    
 }
 
 function checkStr(str1, str2) {
     let arr = [...str2];
     
     for (const char of [...str1]) {
-        for (let i = 0; i < arr.length; i++){
-            if (char === arr[i]) {
-                arr[i] = false;
-                break;
-            }
+        for (let i = 0; i < arr.length; i++) {
+            if (char !== arr[i]) continue;
+            arr[i] = false;
+            break;
         }
     }
     
    return arr.filter((c) => c).length;
-    
 }
