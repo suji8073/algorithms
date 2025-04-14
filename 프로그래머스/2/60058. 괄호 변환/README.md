abe4de9a72b1adc60ac16d14b612db345a0fbb13
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-14 14:39:32 +0900

    [level 2] Title: 괄호 변환, Time: 1.05 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/README.md
+++ 프로그래머스/2/60058. 괄호 변환/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 변환 - 60058 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60058#) 
 
 ### 성능 요약
 
-메모리: 33.8 MB, 시간: 1.46 ms
+메모리: 33.8 MB, 시간: 1.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 14일 13:59:45
+2025년 04월 14일 14:39:29
 
 ### 문제 설명
 
--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -1,4 +1,59 @@
+const map = new Map([
+    ['(', ')']
+])
+
 function solution(p) {
-    var answer = '';
-    return answer;
+    if (checkStr(p)) return p;
+    return getResult(p);
+}
+
+function getResult(p) {
+    let result = '';
+    while (p.length > 0) {
+        const count = [0, 0];
+        let arr = [];
+        for (let i = 0; i < p.length; i++) {
+            if (p[i] === '(') count[0]++;
+            else count[1]++;
+
+            if (count[0] === count[1]) {
+                arr = [p.substr(0, i + 1), p.substr(i + 1)];
+                break;
+            }
+        }
+        
+        if (!checkStr(arr[0])){
+            const v = getResult(arr[1]);
+            const u = [...arr[0]];
+            u.shift();
+            u.pop();
+            
+            const newStr = '(' + v + ')' + u.map(i => i === '(' ? ')' : '(').join('');
+            result += newStr;
+            break;
+        }
+        
+        result += arr[0];
+        p = arr[1];
+    }
+    
+    return result;
+}
+
+function checkStr(str) {
+    const stack = [];
+    
+    for (const char of str) {
+        const len = stack.length;
+        const last = len === 0 ? null : stack[len - 1];
+        
+        if (last === '(' && char === ')') {
+            stack.pop();
+            continue;
+        }
+        
+        stack.push(char);
+    }
+    
+    return stack.length === 0;
 }
