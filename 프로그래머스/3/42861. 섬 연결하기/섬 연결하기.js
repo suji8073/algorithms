684415a1b54644cfbd36d0bc89bca4ba47e0dc7d
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-16 20:14:09 +0900

    [level 3] Title: 섬 연결하기, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,40 +1,36 @@
 function solution(n, costs) {
     costs.sort((a, b) => a[2] - b[2]);
-    const parent = Array.from({length: n}, (_, i) => i);
-
-    let costSum = 0;
+    const parent = Array.from({length: n} , (_, i) => i);
+    
+    let sum = 0;
     for (const [start, end, cost] of costs){
-        if (!isSameParent(start, end)){
-            costSum += cost; 
+        if (!sameParent(start, end)){
+            sum += cost;
             union(start, end);
         }
     }
     
-    return costSum;
+    return sum;
     
-    function union( a, b) {
-        a = getFindParent(a);
-        b = getFindParent(b);
-
-        if (a < b) {
-            parent[b] = a;
-        } else {
-            parent[a] = b;
-        }
-    }
-
-    function isSameParent(x, y){
-        const parentX = getFindParent(x);
-        const parentY = getFindParent(y);
-        return parentX === parentY;
+    function union(x, y){
+        const p1 = findParent(x);
+        const p2 = findParent(y);
+        
+        if (p1 < p2) parent[p2] = p1;
+        else parent[p1] = p2;
     }
     
-    function getFindParent(item) {
-        if (parent[item] === item) {
-            return item;
-        }
-        return parent[item] = getFindParent(parent[item]);
+    function sameParent(x, y){
+        const p1 = findParent(x);
+        const p2 = findParent(y);
+        
+        return p1 === p2;
     }
-   
+    
+    function findParent(t){
+        if (parent[t] === t) return t;
+        return parent[t] = findParent(parent[t]);
+    }
+
 }
 
