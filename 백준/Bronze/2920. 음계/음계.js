5e0044e4611942d281c7395a83da38bd5dc4d88b
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-16 16:46:34 +0900

    [Bronze II] Title: 음계, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.js
+++ 백준/Bronze/2920. 음계/음계.js
@@ -1,10 +1,20 @@
 const fs = require('fs');
-const str = fs.readFileSync('/dev/stdin', 'utf-8').trim().replaceAll(' ', '');
+const arr = fs.readFileSync('/dev/stdin', 'utf-8').trim().split(' ').map(Number);
 
-if (str === '12345678') {
-  console.log('ascending');
-} else if (str === '87654321') {
-  console.log('descending');
-} else {
+let first = arr[0];
+if (first !== 1 && first !== 8 && Math.abs(arr[1] - first) !== 1) {
   console.log('mixed');
+  return;
 }
+
+let isAscending = arr[1] - first;
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] !== first + isAscending) {
+    console.log('mixed');
+    return;
+  }
+  first = arr[i];
+}
+
+console.log(isAscending === 1 ? 'ascending' : 'descending');
