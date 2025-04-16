46eed66cec8126899c18f1699a750dbf8d6540a1
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-16 17:15:18 +0900

    [level 2] Title: 소수 찾기, Time: 4.21 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,12 +1,10 @@
 function solution(numbers) {
-    const numArr = [...numbers];
-    const result = getPermutation(numArr);
-    
+    const result = getPermutation(numbers.split(''));
     return [...new Set(result)].filter(r => checkNum(r)).length;
 }
 
 function checkNum(num){
-    if (num === 1) return false;
+    if (num === 1 || num === 0) return false;
     
     for (let i = 2; i <= Math.sqrt(num); i++){
         if (num % i === 0) return false;
@@ -28,17 +26,17 @@ function getPermutation(arr){
     
     function backTracking(selectNum){
         if (path.length === selectNum) {
-            if (path[0] !== '0') {
-                result.push(Number(path.join('')));
-            }
+            result.push(Number(path.join('')));
             return;
         }
         
+        let prev = null;
         for (let i = 0; i < arr.length; i++){
-            if (!visitied[i]){
+            if (!visitied[i] && prev !== arr[i]){
                 visitied[i] = true;
                 path.push(arr[i]);
                 
+                prev = arr[i];
                 backTracking(selectNum);
                 
                 visitied[i] = false;
