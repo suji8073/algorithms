d0d9c0c9f37df199605417efe2e119024469aab3
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-04-16 20:23:24 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2340 ms, Memory: 255208 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.js
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.js
@@ -5,41 +5,35 @@ const [N, M] = input[0].split(' ').map(Number);
 const arr = input.slice(1).map((row) => row.split(' ').map(Number));
 
 arr.sort((a, b) => a[2] - b[2]);
-
 const parent = Array.from({ length: N + 1 }, (_, i) => i);
 
-const costArr = [];
+const result = [];
 for (const [start, end, cost] of arr) {
   if (!sameParent(start, end)) {
-    costArr.push(cost);
+    result.push(cost);
     union(start, end);
   }
 }
+console.log(
+  result.slice(0, result.length - 1).reduce((acc, cur) => acc + cur, 0)
+);
 
-costArr.pop();
-console.log(costArr.reduce((acc, cur) => acc + cur, 0));
+function union(x, y) {
+  const p1 = findParent(x);
+  const p2 = findParent(y);
 
-function union(x1, x2) {
-  const p1 = findParent(x1);
-  const p2 = findParent(x2);
-
-  if (p1 < p2) {
-    parent[p2] = p1;
-  } else {
-    parent[p1] = p2;
-  }
+  if (p1 < p2) parent[p2] = p1;
+  else parent[p1] = p2;
 }
 
-function sameParent(t1, t2) {
-  const p1 = findParent(t1);
-  const p2 = findParent(t2);
+function sameParent(x, y) {
+  const p1 = findParent(x);
+  const p2 = findParent(y);
 
   return p1 === p2;
 }
 
-function findParent(target) {
-  if (parent[target] === target) {
-    return target;
-  }
-  return (parent[target] = findParent(parent[target]));
+function findParent(t) {
+  if (parent[t] === t) return t;
+  return (parent[t] = findParent(parent[t]));
 }
