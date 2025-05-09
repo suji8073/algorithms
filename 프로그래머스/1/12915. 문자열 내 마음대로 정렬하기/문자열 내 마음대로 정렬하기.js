5f03000c13d1b2dedc0721dc3f7148a1c52cc4de
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-05-09 12:46:50 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,9 +1,9 @@
 function solution(strings, n) {
-    return strings.sort((a,b) => {
-        if (a[n] === b[n]){
-            return a > b ? 1 : -1;
-        } else {
-            return a[n].charCodeAt() - b[n].charCodeAt();
-        }
-    });
+   return strings.sort((a, b) => {
+       if (a[n] === b[n]){
+           return a > b ? 1 : -1;
+       } else {
+           return a[n] > b[n] ? 1 : -1;
+       }
+   })
 }
