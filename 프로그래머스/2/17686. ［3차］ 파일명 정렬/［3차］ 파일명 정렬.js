12814de94488b606a06e98a551856770c375e1b4
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-05-09 21:22:24 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 36.97 ms, Memory: 40.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -0,0 +1,29 @@
+function solution(files) {
+    return files.sort((a, b) => {
+        const [h1, n1] = getSplitText(a);
+        const [h2, n2] = getSplitText(b);
+        
+        if (h1 !== h2) return h1 > h2 ? 1 : -1;
+        if (n1 !== n2) return n1 - n2;
+        
+        return 0;
+    });
+}
+
+function getSplitText(str){
+    let index = 0;
+    let lastIndex = str.length;
+    
+    for (let i = 0; i < str.length; i++){
+         if (index === 0 && !isNaN(str[i]) && str[i] !== ' ') {
+          index = i;
+        }
+
+        if (index !== 0 && (isNaN(str[i]) || str[i] === ' ')) {
+          lastIndex = i;
+          break;
+        }
+    }
+    
+    return [[...str.slice(0, index)].map(s => s.toLowerCase()).join(''), Number(str.slice(index, lastIndex))];
+}
