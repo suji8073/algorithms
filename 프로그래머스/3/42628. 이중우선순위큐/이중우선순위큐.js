d4d809e7c034ed8db9da79faef3271e501f1bfaf
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-06-04 23:48:14 +0900

    [level 3] Title: 이중우선순위큐, Time: 134.23 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,21 +1,15 @@
 function solution(operations) {
     const heap = [];
     
-    for (const operation of operations){
+    for (const operation of operations) {
         const [op, num] = operation.split(' ');
         
-        if (op === 'I'){
+        if (op === 'I') {
             heap.push(Number(num));
             continue;
         }
         
-        if (num === '1'){
-            const maxNum = Math.max(...heap);
-            heap.splice(heap.indexOf(maxNum), 1);
-        } else {
-            const minNum = Math.min(...heap);
-            heap.splice(heap.indexOf(minNum), 1);
-        }
+        heap.splice(heap.indexOf(num === '1' ? Math.max(...heap) : Math.min(...heap)), 1);
     }
     
     return heap.length === 0 ? [0, 0] : [Math.max(...heap), Math.min(...heap)];
