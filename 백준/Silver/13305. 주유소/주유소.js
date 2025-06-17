824d784298bf2b07c999313861a020b6618fafa5
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-06-17 14:21:21 +0900

    [Silver III] Title: 주유소, Time: 260 ms, Memory: 40624 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
 
 const N = Number(input[0]);
-const dist = input[1].split(' ').map(Number);
+const dist = input[1].split(' ').map(BigInt);
 const cost = input[2]
   .split(' ')
-  .map(Number)
-  .slice(0, N - 1);
+  .slice(0, N - 1)
+  .map(BigInt);
 
-let result = 0;
+let result = BigInt(0);
 
 for (let i = 0; i < N - 1; i++) {
   const targetCost = cost[i];
   let totalDist = dist[i];
 
-  while (i + 1 < N - 1 && cost[i + 1] > targetCost) {
+  while (i + 1 < N - 1 && cost[i + 1] >= targetCost) {
     totalDist += dist[i + 1];
     i++;
   }
@@ -22,4 +22,4 @@ for (let i = 0; i < N - 1; i++) {
   result += totalDist * targetCost;
 }
 
-console.log(result);
+console.log(result.toString());
