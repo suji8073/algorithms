802be76a0254e584dc811f4e4b7807862cadd493
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-06-18 19:12:52 +0900

    [Silver V] Title: 문서 검색, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
+
+const str = input[0];
+const target = input[1];
+
+console.log(str.split(target).length - 1);
