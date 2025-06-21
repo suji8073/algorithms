cc70ab07210b5d87c500f86ead65336225488953
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-06-21 22:37:43 +0900

    [level 2] Title: 조이스틱, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,30 +1,28 @@
 function solution(name) {
     const arr = [...name];
-    const len = arr.length;
+    const len = name.length;
     
-    const result = arr.reduce((acc, cur) => acc + getCount(cur), 0);
+    const sum = arr.reduce((acc, cur) => acc + getCount(cur), 0);
     
     if (!arr.includes('A')){
-        return result + len - 1;
+        return sum + len - 1;
     }
     
-    let min = len - 1;
-    
-    for (let i = 0; i < len; i++) {
-        let index = i + 1;
-        while (index < name.length && arr[index] === 'A'){
-            index++;
+    let result = len - 1;
+    for (let i = 0; i < len; i++){
+        let idx = i + 1;
+        while (idx < len && arr[idx] === 'A'){
+            idx++;
         }
-     
-        const path1 = i + (len - index) * 2;
-        const path2 = i + i + len - index;
-        min = Math.min(min, path1, path2);
+        
+        result = Math.min(result, i + (len - idx) * 2, 2 * i + len - idx);
     }
-    return result + min;
+    
+    return result + sum;
 }
 
 function getCount(char){
-    const num = char.charCodeAt();
-    return Math.min(90 - num + 1, num - 65);
+    const charNum = char.charCodeAt();
+    return Math.min(90 - charNum + 1, charNum - 65);
 }
 
