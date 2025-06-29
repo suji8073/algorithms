263532c1e9675c0a4ad0be0159188279a0514641
Suji Chae <63655826+suji8073@users.noreply.github.com>
2025-06-29 19:20:25 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1900 ms, Memory: 229608 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -1,12 +1,42 @@
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+const input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
+
+class Node {
+  constructor(node) {
+    this.cur = node;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+    if (this.length === 0) {
+      this.front = node;
+    } else {
+      this.rear.next = node;
+    }
+    this.rear = node;
+    this.length++;
+  }
+
+  shift() {
+    if (this.length === 0) return false;
+    const returnValue = this.front.cur;
+    this.front = this.front.next;
+    this.length--;
+    return returnValue;
+  }
+}
 
 const [N, M] = input[0].split(' ').map(Number);
 const arr = input.slice(1).map((row) => row.split('').map(Number));
-
-const visited = Array.from({ length: N }, () =>
-  Array.from({ length: M }, () => [false, false])
-);
 
 const vectors = [
   [0, 1],
@@ -15,37 +45,36 @@ const vectors = [
   [-1, 0],
 ];
 
-bfs();
+const queue = new Queue();
+queue.push([0, 0, 1, 0]);
 
-function bfs() {
-  const queue = [[0, 0, 1, 0]];
-  let index = 0;
+const visited = Array.from({ length: N }, () =>
+  Array.from({ length: M }, () => [false, false])
+);
 
-  while (index < queue.length) {
-    const [x, y, count, broken] = queue[index];
-    index++;
+while (queue.length > 0) {
+  const [x, y, count, broken] = queue.shift();
 
-    if (x === N - 1 && y === M - 1) {
-      console.log(count);
-      return;
-    }
+  if (x === N - 1 && y === M - 1) {
+    console.log(count);
+    return;
+  }
 
-    for (const vector of vectors) {
-      const nx = x + vector[0];
-      const ny = y + vector[1];
+  for (const [dx, dy] of vectors) {
+    const nx = dx + x;
+    const ny = dy + y;
 
-      if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+    if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+    if (visited[nx][ny][broken]) continue;
 
-      if (broken === 0 && arr[nx][ny] === 1 && !visited[nx][ny][1]) {
-        visited[nx][ny][1] = true;
-        queue.push([nx, ny, count + 1, 1]);
-      }
-
-      if (arr[nx][ny] === 0 && !visited[nx][ny][broken]) {
-        visited[nx][ny][broken] = true;
-        queue.push([nx, ny, count + 1, broken]);
-      }
+    if (arr[nx][ny] === 0) {
+      visited[nx][ny][broken] = true;
+      queue.push([nx, ny, count + 1, broken]);
+    } else if (arr[nx][ny] === 1 && broken === 0) {
+      visited[nx][ny][1] = true;
+      queue.push([nx, ny, count + 1, 1]);
     }
   }
-  console.log(-1);
 }
+
+console.log(-1);
